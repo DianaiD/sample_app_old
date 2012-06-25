87d5ce8d6f3d275d67c3e78b5951145e6bf9e2ba
Diana <diana.poudel@gmail.com>
2012-06-25 20:46:11 +0300

    Finish sign in

---
+++ db/migrate/20120625163504_add_member_token_to_users.rb
@@ -0,0 +1,6 @@
+class AddMemberTokenToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :remember_token, :string
+    add_index  :users, :remember_token
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -39,6 +39,14 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+      describe "after saving the user" do
+        before { click_button submit}
+        it {should have_link('Sign out') }
+      end
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
   end
 end
